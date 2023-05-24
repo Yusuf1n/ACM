a6b9742c010fbf9afac7f9dac591c9459fe6cad2
Yusuf Naheem <Yusuf.Naheem@vercoglobal.com>
2023-05-24 15:55:09 +0100

    Added CustomerRepositoryTest class for unit tests and populated RetieveValid test

---
+++ Tests/ACM.BLTest/CustomerRepositoryTest.cs
@@ -0,0 +1,31 @@
+﻿using ACM.BL;
+
+namespace ACM.BLTest;
+
+[TestClass]
+public class CustomerRepositoryTest
+{
+
+    [TestMethod]
+    public void RetrieveValid()
+    {
+        // Arrange
+        var customerRepository = new CustomerRepository();
+        var expected = new Customer(1)
+        {
+            EmailAddress = "yusufnaheem1@gmail.com",
+            FirstName = "Yusuf",
+            LastName = "Naheem"
+        };
+
+        // Act
+        var actual = customerRepository.Retrieve(1);
+
+        // Assert
+        Assert.AreEqual(expected.CustomerId, actual.CustomerId);
+        Assert.AreEqual(expected.EmailAddress, actual.EmailAddress);
+        Assert.AreEqual(expected.FirstName, actual.FirstName);
+        Assert.AreEqual(expected.LastName, actual.LastName);
+    }
+
+}
